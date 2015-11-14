6cc2a302febd9ddabfae8ce038ee0966e22170e0
Christopher Kwai-Pun <chris.kwaipun@gmail.com>
2015-11-14 17:37:27 -0500

    wat

--- services/results.js
+++ services/results.js
@@ -1,16 +1,17 @@
-angular.module('Gainzzz')
+angular.module('Gainzzz', [])
 
-.factory('results', function(){
-	var workouts = 
+.factory('results', function showResults(diet, aid, work, stretch){
+	console.log('test');
+	var workouts =
 		{"exercises": [
-			{"description": "something useful", 
-			"name": "bicep curl"}, 
+			{"description": "something useful",
+			"name": "bicep curl"},
 
-			{"description": "something also useful", 
+			{"description": "something also useful",
 			"name": "pec fly"}
-		], 
+		],
 		"muscle": {
-			"description": "something useful", 
+			"description": "something useful",
 			"name": "biceps"
 		}};
 });
